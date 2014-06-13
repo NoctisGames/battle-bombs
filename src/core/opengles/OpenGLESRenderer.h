f1efb538c9af5ca0fafd067f669d11f884fd621c
Stephen Gowen <sgowen@technegames.com>
2014-06-13 13:02:21 -0500

    Moving method declarations around to match the order they are called

--- src/core/opengles/OpenGLESRenderer.h
+++ src/core/opengles/OpenGLESRenderer.h
@@ -29,11 +29,11 @@ public:
     
     virtual void renderWorldForeground(std::vector<std::unique_ptr<InsideBlock>> &insideBlocks, std::vector<std::unique_ptr<BreakableBlock>> &breakableBlocks, std::vector<std::unique_ptr<PowerUp>> &powerUps);
     
-    virtual void renderPlayers(std::vector<std::unique_ptr<PlayerDynamicGameObject>> &players);
-    
     virtual void renderBombs(std::vector<std::unique_ptr<BombGameObject>> &bombs);
     
     virtual void renderExplosions(std::vector<std::unique_ptr<Explosion>> &explosions);
+    
+    virtual void renderPlayers(std::vector<std::unique_ptr<PlayerDynamicGameObject>> &players);
     
     virtual void renderInterface();
     
