d5baf68dd74604dd3c6c9c9fba31c8856ea3e106
Stephen Gowen <sgowen@technegames.com>
2014-06-19 13:17:48 -0500

    Updating AppWarp iOS SDK so that it is now compatible with iPad

--- src/platform/ios/MainViewController.h
+++ src/platform/ios/MainViewController.h
@@ -7,6 +7,7 @@
 //
 
 #define APPWARP_API_KEY         @"b9d7c098-5fb8-4182-8"
+#define APPWARP_SECRET_KEY      @"139f6094-4e04-4041-9"
 #define APPWARP_HOST_ADDRESS    @"191.234.48.70"
 
 #import <AppWarp_iOS_SDK/AppWarp_iOS_SDK.h>
