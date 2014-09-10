48dbb7b144078a376ed366413969fdf7055e1d2b
Stephen Gowen <sgowen@technegames.com>
2014-09-09 22:39:26 -0500

    Adding PlayerRow class.
    The Waiting For Server interface will keep track of the players in the
    room (and the platforms they are on) via these objects.

---
+++ src/core/PlayerRow.cpp
@@ -0,0 +1,9 @@
+//
+//  PlayerRow.cpp
+//  battlebombs
+//
+//  Created by Stephen Gowen on 9/9/14.
+//  Copyright (c) 2014 Techne Games. All rights reserved.
+//
+
+#include "PlayerRow.h"
---
+++ src/core/PlayerRow.h
@@ -0,0 +1,14 @@
+//
+//  PlayerRow.h
+//  battlebombs
+//
+//  Created by Stephen Gowen on 9/9/14.
+//  Copyright (c) 2014 Techne Games. All rights reserved.
+//
+
+#ifndef __battlebombs__PlayerRow__
+#define __battlebombs__PlayerRow__
+
+#include <iostream>
+
+#endif /* defined(__battlebombs__PlayerRow__) */
