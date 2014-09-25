ff31aa622fbdb0119eba2361b4b086875c3dc9e5
Stephen Gowen <sgowen@technegames.com>
2014-09-25 02:20:49 -0500

    asasd

--- src/platform/windows-phone/BattleBombs/BattleBombs/BattleBombsComp/Helpers/XAudio2SoundPlayer.h
+++ src/platform/windows-phone/BattleBombs/BattleBombs/BattleBombsComp/Helpers/XAudio2SoundPlayer.h
@@ -7,33 +7,30 @@
 
 #pragma once
 
-
-#include "Direct3DBase.h"
-
 #include "mmreg.h"
 #include <vector>
 #include <memory>
 
 class XAudio2SoundPlayer
 {
-    struct ImplData;
+	struct ImplData;
 
-    public:
-        XAudio2SoundPlayer(uint32 sampleRate);
-        ~XAudio2SoundPlayer();
+public:
+	XAudio2SoundPlayer(uint32 sampleRate);
+	~XAudio2SoundPlayer();
 
-        size_t AddSound(_In_ WAVEFORMATEX* format, _In_ Platform::Array<byte>^ data);
-        bool   PlaySound(size_t index);
-        bool   StopSound(size_t index);
-        bool   IsSoundPlaying(size_t index) const;
-        size_t GetSoundCount() const;
+	size_t AddSound(_In_ WAVEFORMATEX* format, _In_ Platform::Array<byte>^ data);
+	bool   PlaySound(size_t index);
+	bool   StopSound(size_t index);
+	bool   IsSoundPlaying(size_t index) const;
+	size_t GetSoundCount() const;
 
-        void Suspend();
-        void Resume();
+	void Suspend();
+	void Resume();
 
-    private:
-        interface IXAudio2*                     m_audioEngine;
-        interface IXAudio2MasteringVoice*       m_masteringVoice;
-        std::vector<std::shared_ptr<ImplData>>  m_soundList;
+private:
+	interface IXAudio2*                     m_audioEngine;
+	interface IXAudio2MasteringVoice*       m_masteringVoice;
+	std::vector<std::shared_ptr<ImplData>>  m_soundList;
 };
 
