2c737c71e6bee8deac9f6b07f897c0fcfd590a1b
Stephen Gowen <sgowen@technegames.com>
2014-10-05 20:17:28 +0000

    Preventing Ice Balls from hitting inner map border regions

--- src/platform/server/src/com/technegames/battlebombs/BbRoomAdaptor.java
+++ src/platform/server/src/com/technegames/battlebombs/BbRoomAdaptor.java
@@ -236,7 +236,7 @@ public final class BbRoomAdaptor extends BaseRoomAdaptor
                             }
                         }
 
-                        if (!_isSuddenDeathMode && _numSecondsLeftForRound <= 60)
+                        if (!_isSuddenDeathMode && _numSecondsLeftForRound <= 174)
                         {
                             try
                             {
@@ -394,6 +394,8 @@ public final class BbRoomAdaptor extends BaseRoomAdaptor
                 {
                     mapType = 0;
                 }
+
+                mapType = 2;
 
                 init(_room.getId(), _inGameUserSessionDataMap.size(), mapType);
 
@@ -554,30 +556,54 @@ public final class BbRoomAdaptor extends BaseRoomAdaptor
         {
             for (int i = inset; i < (NUM_GRID_CELLS_PER_ROW - inset); i++)
             {
-                iceBallXValues += "" + i + ",";
-                iceBallYValues += "" + (GRID_CELL_NUM_ROWS - 1 - inset) + ",";
-                numIceBalls++;
+                int x = i;
+                int y = (GRID_CELL_NUM_ROWS - 1 - inset);
+
+                if ((x > 2 || y > 2) || (x < (NUM_GRID_CELLS_PER_ROW - 3) || y > 2))
+                {
+                    iceBallXValues += "" + x + ",";
+                    iceBallYValues += "" + y + ",";
+                    numIceBalls++;
+                }
             }
 
             for (int i = (GRID_CELL_NUM_ROWS - 2 - inset); i >= inset; i--)
             {
-                iceBallXValues += "" + (NUM_GRID_CELLS_PER_ROW - 1 - inset) + ",";
-                iceBallYValues += "" + i + ",";
-                numIceBalls++;
+                int x = (NUM_GRID_CELLS_PER_ROW - 1 - inset);
+                int y = i;
+
+                if ((x > 2 || y > 2) || (x < (NUM_GRID_CELLS_PER_ROW - 3) || y > 2))
+                {
+                    iceBallXValues += "" + x + ",";
+                    iceBallYValues += "" + y + ",";
+                    numIceBalls++;
+                }
             }
 
             for (int i = (NUM_GRID_CELLS_PER_ROW - 2 - inset); i >= inset; i--)
             {
-                iceBallXValues += "" + i + ",";
-                iceBallYValues += "" + inset + ",";
-                numIceBalls++;
+                int x = i;
+                int y = inset;
+
+                if ((x > 2 || y > 2) || (x < (NUM_GRID_CELLS_PER_ROW - 3) || y > 2))
+                {
+                    iceBallXValues += "" + x + ",";
+                    iceBallYValues += "" + y + ",";
+                    numIceBalls++;
+                }
             }
 
             for (int i = (1 + inset); i < (GRID_CELL_NUM_ROWS - 1 - inset); i++)
             {
-                iceBallXValues += "" + inset + ",";
-                iceBallYValues += "" + i + ",";
-                numIceBalls++;
+                int x = inset;
+                int y = i;
+
+                if ((x > 2 || y > 2) || (x < (NUM_GRID_CELLS_PER_ROW - 3) || y > 2))
+                {
+                    iceBallXValues += "" + x + ",";
+                    iceBallYValues += "" + y + ",";
+                    numIceBalls++;
+                }
             }
         }
 
