5ab9794b9646bb6c4451487d69f0ce9dfdd4598f
Stephen Gowen <sgowen@technegames.com>
2014-10-14 15:20:38 -0500

    Updating constants and other params for v1.2

--- src/platform/windows-phone/BattleBombs/BattleBombs/BattleBombs/ApiConstants.cs
+++ src/platform/windows-phone/BattleBombs/BattleBombs/BattleBombs/ApiConstants.cs
@@ -8,6 +8,6 @@ namespace BattleBombs
 {
     public class ApiConstants
     {
-        public static String SERVER_STATUS_URL = "https://s3.amazonaws.com/battlebombs/bb_1.1.0.json";
+        public static String SERVER_STATUS_URL = "https://s3.amazonaws.com/battlebombs/bb_1.2.0.json";
     }
 }
--- src/platform/windows-phone/BattleBombs/BattleBombs/BattleBombs/AppWarpConstants.cs
+++ src/platform/windows-phone/BattleBombs/BattleBombs/BattleBombs/AppWarpConstants.cs
@@ -14,9 +14,9 @@ namespace BattleBombs
 {
     public class AppWarpConstants
     {
-        public static String APPWARP_APP_KEY = "e4207af0-de78-496d-8";
+        public static String APPWARP_APP_KEY = "475c20d0-07d6-46ad-a";
         public static String APPWARP_SECRET_KEY = "139f6094-4e04-4041-9";
-        public static String APPWARP_HOST_ADDRESS = "191.234.54.124";
-        public static String APPWARP_AUTH_DATA = "T3chn3G4m35_1.1.0";
+        public static String APPWARP_HOST_ADDRESS = "23.99.132.2";
+        public static String APPWARP_AUTH_DATA = "T3chn3G4m35_1.2.0";
     }
 }
