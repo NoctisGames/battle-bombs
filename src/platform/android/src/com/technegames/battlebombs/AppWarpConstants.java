afc715755aed6e43eab1a5d6c6f9e404d8318e90
Stephen Gowen <sgowen@technegames.com>
2014-12-07 13:47:49 -0600

    v1.3 prep

--- src/platform/android/src/com/technegames/battlebombs/AppWarpConstants.java
+++ src/platform/android/src/com/technegames/battlebombs/AppWarpConstants.java
@@ -2,8 +2,8 @@ package com.technegames.battlebombs;
 
 public final class AppWarpConstants
 {
-    public static final String APPWARP_APP_KEY = "475c20d0-07d6-46ad-a";
+    public static final String APPWARP_APP_KEY = "e4207af0-de78-496d-8";
     public static final String APPWARP_SECRET_KEY = "139f6094-4e04-4041-9";
-    public static final String APPWARP_HOST_ADDRESS = "23.99.132.2";
-    public static final String APPWARP_AUTH_DATA = "T3chn3G4m35_1.2.0";
+    public static final String APPWARP_HOST_ADDRESS = "191.234.54.124";
+    public static final String APPWARP_AUTH_DATA = "GowenGameDev_1.3.0";
 }
--- src/platform/ios/TGApiManager.m
+++ src/platform/ios/TGApiManager.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Techne Games. All rights reserved.
 //
 
-#define SERVER_STATUS_URL @"https://s3.amazonaws.com/battlebombs/bb_1.2.0.json"
+#define SERVER_STATUS_URL @"https://s3.amazonaws.com/battlebombs/bb_1.3.0.json"
 
 #import "TGApiManager.h"
 
