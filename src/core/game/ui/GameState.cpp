7df2f4dfe2c35ed62c0f72761c154951d2af8e82
Stephen Gowen <sgowen@technegames.com>
2014-12-29 12:44:19 -0600

    wtf, Windows phone gaying it up

--- src/core/game/ui/GameState.cpp
+++ src/core/game/ui/GameState.cpp
@@ -15,61 +15,64 @@
 
 void GameState::update(GameScreen *gameScreen, float deltaTime)
 {
-    processServerMessages(gameScreen);
+	processServerMessages(gameScreen);
 }
 
 void GameState::processServerMessages(GameScreen *gameScreen)
 {
-    std::vector<const char *> serverMessages = gameScreen->getGameListener()->freeServerMessages();
-    
-    for (std::vector<const char *>::iterator itr = serverMessages.begin(); itr != serverMessages.end(); itr++)
-    {
-        rapidjson::Document d;
-        d.Parse<0>(*itr);
-        if(d.IsObject())
-        {
-            static const char *eventTypeKey = "eventType";
-            
-            if(d.HasMember(eventTypeKey))
-            {
-                int eventType = d[eventTypeKey].GetInt();
-                
-                processServerMessage(gameScreen, d, eventType);
-            }
-        }
-    }
+	std::vector<const char *> serverMessages = gameScreen->getGameListener()->freeServerMessages();
+
+	if (serverMessages.size() > 0)
+	{
+		for (std::vector<const char *>::iterator itr = serverMessages.begin(); itr != serverMessages.end(); itr++)
+		{
+			rapidjson::Document d;
+			d.Parse<0>(*itr);
+			if (d.IsObject())
+			{
+				static const char *eventTypeKey = "eventType";
+
+				if (d.HasMember(eventTypeKey))
+				{
+					int eventType = d[eventTypeKey].GetInt();
+
+					processServerMessage(gameScreen, d, eventType);
+				}
+			}
+		}
+	}
 }
 
 bool GameState::shouldUpdateGameInterface()
 {
-    return false;
+	return false;
 }
 
 bool GameState::shouldUpdateSpectatorInterface()
 {
-    return false;
+	return false;
 }
 
 bool GameState::shouldHandlePlayerDataUpdate(GameScreen *gameScreen, short playerIndex, bool isBeginGame)
 {
-    return isBeginGame || playerIndex != gameScreen->getPlayerIndex();
+	return isBeginGame || playerIndex != gameScreen->getPlayerIndex();
 }
 
 #pragma mark <Protected>
 
 void GameState::updateTimers(GameScreen *gameScreen, float deltaTime)
 {
-    gameScreen->setTimeSinceLastClientEvent(gameScreen->getTimeSinceLastClientEvent() + deltaTime);
-    
-    gameScreen->setTimeSinceLastServerUpdate(gameScreen->getTimeSinceLastServerUpdate() + deltaTime);
-    
-    if(gameScreen->getTimeSinceLastServerUpdate() > 8)
-    {
-        gameScreen->setScreenState(SCREEN_STATE_CONNECTION_ERROR);
-    }
+	gameScreen->setTimeSinceLastClientEvent(gameScreen->getTimeSinceLastClientEvent() + deltaTime);
+
+	gameScreen->setTimeSinceLastServerUpdate(gameScreen->getTimeSinceLastServerUpdate() + deltaTime);
+
+	if (gameScreen->getTimeSinceLastServerUpdate() > 8)
+	{
+		gameScreen->setScreenState(SCREEN_STATE_CONNECTION_ERROR);
+	}
 }
 
 void GameState::setGameState(GameScreen *gameScreen, int gameState)
 {
-    gameScreen->setGameState(GameStateFactory::getInstance().createGameState(gameState, false));
+	gameScreen->setGameState(GameStateFactory::getInstance().createGameState(gameState, false));
 }
