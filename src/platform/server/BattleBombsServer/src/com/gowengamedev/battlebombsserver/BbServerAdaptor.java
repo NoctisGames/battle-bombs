f51ff593d6c52b336cf073eb25a44fd950e0a34a
Stephen Gowen <dev.sgowen@gmail.com>
2015-09-08 15:01:04 -0500

    Removing machos and windows platforms (they were never actually implemented), prepping battle bombs server code for the Ubuntu platform, and removing random num generator project

---
+++ src/platform/server/BattleBombsServer/src/com/gowengamedev/battlebombsserver/BbServerAdaptor.java
@@ -0,0 +1,19 @@
+package com.gowengamedev.battlebombsserver;
+
+import com.shephertz.app42.server.idomain.BaseServerAdaptor;
+import com.shephertz.app42.server.idomain.IZone;
+
+/**
+ *
+ * @author sgowen
+ */
+public final class BbServerAdaptor extends BaseServerAdaptor
+{
+    @Override
+    public void onZoneCreated(IZone zone)
+    {
+        System.out.println("Zone Created " + zone.getName() + " with key " + zone.getAppKey());
+        
+        zone.setAdaptor(new BbZoneAdaptor(zone));
+    }
+}
